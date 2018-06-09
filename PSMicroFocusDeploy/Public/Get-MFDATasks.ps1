858d5641e3f26e2e717dac1690b17c8dcf06b734
akevinlee <klee@serena.com>
2018-06-09 10:43:01 +0100

    Object changes

--- PSMicroFocusDeploy/Public/Get-MFDATasks.ps1
+++ PSMicroFocusDeploy/Public/Get-MFDATasks.ps1
@@ -22,7 +22,9 @@ function Get-MFDATasks {
   $Body = @{
   }
 
-  $TasksUrl = $global:MFDAUrl + "/rest/approval/task/tasksForUserGroupedByApproval"
+  $TasksURL = $global:MFDAUrl + "/rest/approval/task/tasksForUser?orderField=startDate&pageNumber=1&rowsPerPage=99999&sortType=asc"
+  #For DA 6.2+
+  #$TasksUrl = $global:MFDAUrl + "/rest/approval/task/tasksForUserGroupedByApproval"
   Write-Debug "Retrieving tasks via $TasksUrl"
 
   $Headers = @{
@@ -33,26 +35,31 @@ function Get-MFDATasks {
   [bool]$moreTasks = $True
   while ($moreTasks) {
 
-    $Tasks = Invoke-RestMethod -Method Get -Uri $TasksUrl -Headers $Headers -Body $Body -ContentType 'application/json'
-    
-    If ($Tasks) {
-      #Write-Host 'Select a [#] from the options below!' `n -ForegroundColor Black -BackgroundColor Green
+    $Tasks = Invoke-RestMethod -Method Get -Uri $TasksUrl -Headers $Headers -Body $Body -ContentType "application/json"
 
+    If ($Tasks) {
+      $taskCollection = @()
+      $taskName = $Null
+      $taskType = $Null
+      $requestedDate = $Null
+      $requestedBy = $Null
+      $versions = $Null
+      $environment = $Null
       $i = 0 
       ForEach ($Task in $Tasks) {
-        Write-Host "[$i] -> $($task.name) [$($task.type)]"
-        Switch ($task.type) {
+        $taskName = $Task.name
+        $taskType = $Task.type
+        Switch ($Task.type) {
           "approval" {
-            $requestDate = (Get-Date '1/1/1970').AddMilliseconds($task.applicationProcessRequest.submittedTime)
-            Write-Host `t"Requested on: [$requestDate], by [$($task.applicationProcessRequest.userName)]"
+            #componentProcessRequest
+            $requestedBy = $task.applicationProcessRequest.userName
+            $requestedDate = (Get-Date '1/1/1970').AddMilliseconds($task.applicationProcessRequest.submittedTime)
             if ($task.applicationProcessRequest.snapshot) {
-              Write-Host `t"Deploying snapshot: [$($task.applicationProcessRequest.snapshot.name)], to [$($task.applicationProcessRequest.environment.name)]"
+              $versions = $task.applicationProcessRequest.snapshot.name
             } else {
-              $versions = $null
               ForEach ($version in $task.applicationProcessRequest.versions) {
                 $versions = $versions + $version.component.name + ":" + $version.name + ", "
               }
-              Write-Host `t"Deploying version(s); [$($versions)], to [$($task.applicationProcessRequest.environment.name)]"
             }  
             break;
           }
@@ -73,8 +80,12 @@ function Get-MFDATasks {
             break;
           }
         }
+        Write-Host $requestDate $requestedBy
+        $taskCollection += New-Task -Index $i -Name $taskName -Type $taskType -RequestedDate $requestedDate -RequestedBy $requestedBy -Versions $versions -Environment $environment 
         $i++
       }
+
+      $taskCollection
 
       #Write-Host `n
 
@@ -94,7 +105,7 @@ function Get-MFDATasks {
           $ApprovalUrl = $global:MFDAUrl + "/rest/approval/task/$($Tasks[$_].id)/close"
           $ApprovalAction = "passed"
           Write-Debug $ApprovalUrl
-          if ($Action.Substring(0,1).ToLower() -eq "r") { $ApprovalAction = "failed" }
+          if ($taskAction.Substring(0,1).ToLower() -eq "r") { $ApprovalAction = "failed" }
           $Body = @{
             comment=$taskComment
             passFail=$ApprovalAction
@@ -118,4 +129,9 @@ function Get-MFDATasks {
   #    AppProcessName=$_.applicationProcessRequest.applicationProcess.name;
   #    EnvironmentName=$_.applicationProcessRequest.environment.name}} | Format-Table
  
+}
+
+Function New-Task ($Index ='0',$Name='Name',$Type='approval',$RequestedDate='',$RequestedBy='',$Versions='',$Environment='')
+{
+   New-Object -TypeName psObject -Property @{Index=$index; Name=$name; Type=$type; RequestedDate=$requested; RequestBy=$requestedBy; Versions=$versions; Environment=$environment}
 }
